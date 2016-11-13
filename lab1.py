839c1f60ef63df94e156f0c6cdfc62e64c38f56b
Mellokel <near.csd.me@gmail.com>
2016-11-13 15:15:36 +0700

    Revert "PL-1"
    This reverts commit ec8ef9323651a130a2de69aa054e1b4be100b4cb.

--- lab1.py
+++ lab1.py
@@ -10,36 +10,39 @@ reference=[]
 
 time.clock()
 def lookfor(d):
-    result=re.findall(r'\b[\w\d]+@[\w\d.]+', d.text)
+    result=re.findall(r'\b[\w_.+-]+@[\w.]+', d.text)
     k=0
     for i in range(len(result)):
             for j in range(len(emails)):
                 if result[i]==emails[j]:
-                                    k=1
+                    k=1
             if k==0:
                 emails.append(result[i])
             k=0
     result=[]
+
+#print(time.clock())
+
 def page(d, deep):
-    reference.append(d.url)
+    reference.append(d.StartRef)
 
     deep+=1
     deep1=deep
     lookfor(d)
 
-    newref=StartRef + '([A-z0-9-/+=&?_$]+)'
+    newref=StartRef + '([\w-/+=&?_$]+)'
     refs = re.findall(r'<a href=[\"\']'+newref + '[\"\']>', d.text)
     if deep < maxdeep:
-            z=0
+            k=0
             for i in range(len(refs)):
-                    for j in range(len(reference)):
-                            if StartRef+refs[i]==reference[j]:
-                                        z=1
-                    if z==0:
-                            dop=requests.get(StartRef +refs[i])
-                            page(dop, deep)
-                    z=0
-            deep=deep1
+                for j in range(len(reference)):
+                    if StartRef+refs[i]==reference[j]:
+                        k=1
+                    if k==0:
+                        dop=requests.get(StartRef +refs[i])
+                        page(dop, deep)
+                    k=0
+                deep=deep1
 page(d,0)
 
 
